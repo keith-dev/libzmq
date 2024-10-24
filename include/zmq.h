7cc8d78fc5f9daea0611c010740c0a417dff2d22
Keith Williams <keith.dev.uk@gmail.com>
2024-10-24 21:12:45 +0100

    add raw socket

--- include/zmq.h
+++ include/zmq.h
@@ -267,7 +267,6 @@ ZMQ_EXPORT const char *zmq_msg_gets (const zmq_msg_t *msg_,
 #define ZMQ_XPUB 9
 #define ZMQ_XSUB 10
 #define ZMQ_STREAM 11
-#define ZMQ_RAW 12
 
 /*  Deprecated aliases                                                        */
 #define ZMQ_XREQ ZMQ_DEALER
@@ -350,6 +349,7 @@ ZMQ_EXPORT const char *zmq_msg_gets (const zmq_msg_t *msg_,
 #define ZMQ_GSSAPI_PRINCIPAL_NAMETYPE 90
 #define ZMQ_GSSAPI_SERVICE_PRINCIPAL_NAMETYPE 91
 #define ZMQ_BINDTODEVICE 92
+#define ZMQ_RAW_NOTIFY 73
 
 /*  Message options                                                           */
 #define ZMQ_MORE 1
@@ -629,6 +629,7 @@ ZMQ_EXPORT void zmq_threadclose (void *thread_);
 #define ZMQ_DGRAM 18
 #define ZMQ_PEER 19
 #define ZMQ_CHANNEL 20
+#define ZMQ_RAW 21
 
 /*  DRAFT Socket options.                                                     */
 #define ZMQ_ZAP_ENFORCE_DOMAIN 93
